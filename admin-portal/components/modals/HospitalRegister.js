380234875b0cae17659c24e2530fc7ffa0461112
feyselmubarek <feyselmubarek@gmail.com>
2023-05-21 10:53:16 +0100

    cleanup admin

--- admin-portal/components/modals/HospitalRegister.js
+++ admin-portal/components/modals/HospitalRegister.js
@@ -4,41 +4,17 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalFooter,
   Box,
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  FormControl,
-  Input,
-  Select,
-  Text,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { GiHospitalCross } from "react-icons/gi";
 
-import { createHealthCareCenter } from "../../firebase/healthServices";
-import PasswordInput from "../PasswordInput";
+import AddHealthcare from "../forms/AddHealthcare";
 
 function HospitalRegister() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [type, setType] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleOnClose = () => {
-    setIsLoading(false);
-    onClose();
-  };
-
-  const handleOnAdd = () => {
-    const center = { name, address, type, email, password };
-    setIsLoading(true);
-    createHealthCareCenter(center, handleOnClose);
-  };
 
   return (
     <>
@@ -53,80 +29,8 @@ function HospitalRegister() {
           <ModalCloseButton />
 
           <ModalBody pb={6}>
-            <Text fontSize="md" color="gray.500" mb="2">
-              Basic Information
-            </Text>
-
-            <FormControl>
-              <Input
-                placeholder="Name of the center"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <Input
-                placeholder="Address of the center"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <Select
-                value={type}
-                placeholder="Select type of center"
-                onChange={(e) => setType(e.target.value)}
-              >
-                <option value="referal">Referal Hospital</option>
-                <option value="hospital">Hospital</option>
-                <option value="clinic">Clinic</option>
-                <option value="lab">Laboratory</option>
-                <option value="nursing">Nursing Home</option>
-              </Select>
-            </FormControl>
-
-            <Text fontSize="md" color="gray.500" mt="4">
-              Login Credential
-            </Text>
-            <Text fontSize="xs" mb="2" color="gray.400">
-              The password should be changed after first login
-            </Text>
-
-            <FormControl>
-              <Input
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <PasswordInput
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormControl>
+            <AddHealthcare onClose={onClose} />
           </ModalBody>
-
-          <ModalFooter>
-            <Button
-              disabled={isLoading}
-              variant="ghost"
-              mr={3}
-              onClick={onClose}
-            >
-              Cancel
-            </Button>
-            <Button
-              colorScheme="brand"
-              isLoading={isLoading}
-              onClick={handleOnAdd}
-            >
-              Add
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
--- admin-portal/pages/_app.js
+++ admin-portal/pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
+import "../styles/globals.css"
 
 import Layout from "../components/Layout";
 import ProtectedRoute from "../components/wrappers/ProtectedRoute";
